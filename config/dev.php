e8547cd96b1b9cadf17935f6c3f3abb627377fbc
hjhasflkjfh <42581541+jkgthb32@users.noreply.github.com>
2026-03-19 22:22:12 +0100

    Update dev.php

--- config/dev.php
+++ config/dev.php
@@ -4,7 +4,7 @@ return [
     'author' => 'Shaun Parkison',
     'version' => '0.9.28',
     'dev_version' => '0.10.18-dev',
-    'experimental_version' => '0.11.1-exp',
+    'experimental_version' => '0.11.1-exp-jk1',
     'repo' => 'm3ue/m3u-editor',
     'docs_url' => 'https://m3ue.sparkison.dev',
     'donate' => 'https://buymeacoffee.com/shparkison',
